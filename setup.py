1c217c31b7b6e8411541dc970b10bf311c3c3767
digitaluniverse <contact@mattdaniele.com>
2024-06-16 21:57:00 -0700

    added readme and updated setup

--- setup.py
+++ setup.py
@@ -12,4 +12,16 @@ setup(
     install_requires=[
         # List dependencies here
     ],
+    author='Matt Daniele',
+    author_email='contact@mattdaniele.com',
+    description='A tool to pack and unpack directories to/from .txt files with Git branch support',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/digitaluniverse/packtxt',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
 )
